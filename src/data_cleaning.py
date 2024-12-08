84053556cba453e991c80dad6e6e3feb6f932ada
Abel <abelmarie1990@gmail.com>
2024-12-08 09:19:54 -0300

    updating data_cleaning.py to include a function for checking missing values, checking duplicates and check negative values

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -12,6 +12,39 @@ def load_data(file_path):
     except Exception as e:
         print(f"Error loading data: {e}")
         return None
+    
+
+def check_missing_values(df):
+    
+    missing_values = df.isnull().sum()
+    missing_percentage = (df.isnull().mean()) * 100
+    missing_info = pd.DataFrame({
+        'Missing Values': missing_values,
+        'Percentage': missing_percentage
+    })
+    return missing_info
+
+
+def check_duplicates(df):
+ 
+    duplicates = df.duplicated().sum()
+    return duplicates
+
+
+def check_negative_values(df, columns):
+   
+    negative_values = {}
+    for column in columns:
+        if column in df.columns:
+            negative_values[column] = (df[column] < 0).sum()
+        else:
+            negative_values[column] = None  
+
+    negative_values_df = pd.DataFrame(list(negative_values.items()), columns=['Column', 'Negative Count'])
+    return negative_values_df
+
+
+
 
 def clean_missing_values(df, strategy='mean'):
     """
